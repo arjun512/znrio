b46f2b43eae616227cde74064867576cd2984294
lijiaocn <lijiaocn@foxmail.com>
2017-03-16 21:51:10 +0800

    m

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 cd article;jekyll build  -d /www/znr/;cd ..
 cd shop;jekyll build  -d /www/znr/shop;cd ..
 cd product;jekyll build  -d /www/znr/product;cd ..
+cd wangke;jekyll build  -d /www/znr/wangke;cd ..
 
 cd /www; ./push_znr.sh
